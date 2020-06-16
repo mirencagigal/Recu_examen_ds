bffaf09a70c3a2c78f5c01c6a76fae69c60db7f0
Miren <miren.cagigal@alumni.mondragon.edu>
2020-06-16 10:11:22 +0200

    Identificamos las sociedades limitadas

--- Carga fichero.R
+++ Carga fichero.R
@@ -10,3 +10,13 @@ names(muestra01) <- c("NifCif", "nombre", "genero")
 names(muestra01)
 
 #cambio los nombres de las variables
+
+
+# Identificadion Sociedades Limitadas -------------------------------------
+library(stringr)
+regexp <- "([[:alpha:]]{1})([[:digit:]]{8})"
+grepl(pattern = regexp, x = muestra01$NifCif) #no hay ningun registro que empiece por una letra y tenga 8 digitos
+
+str_detect(muestra01$NifCif, "^B")
+
+#No hay ningun registro que empiece por la letra B y tenga 8 digitod
